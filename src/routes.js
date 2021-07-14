bdd41a304fccd553ccc15a94221d035de560971d
fabinhoizzy <fabinhoevil16@hotmail.com>
2021-07-14 13:23:00 -0300

    Instalando e configurando react-router-dom

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import Repositories from './repositories';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path='/repositories' component={Repositories} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
